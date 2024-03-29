b3c95aa4f2ba2dd9a294b4dbf20d018505aa863e
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-03-29 12:41:38 +0300

    started to create entities

---
+++ src/main/java/com/hakan/restaurant/backend/entities/concretes/Like.java
@@ -0,0 +1,15 @@
+package com.hakan.restaurant.backend.entities.concretes;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "likes")
+@Entity
+public class Like {
+}
---
+++ src/main/java/com/hakan/restaurant/backend/entities/concretes/Meal.java
@@ -0,0 +1,21 @@
+package com.hakan.restaurant.backend.entities.concretes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "meals")
+@Entity
+public class Meal {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+    private String mealName;
+    private String category;
+
+}
